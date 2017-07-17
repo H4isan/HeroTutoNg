8d8aa17e9c44ef30fc213fcd4c6cc6ac18229725
Harlen Giraldo <harlen.giraldo@yuxiglobal.com>
2017-07-17 09:16:53 -0500

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HeroDetailComponent } from './hero-detail.component';
 import { FormsModule } from '@angular/forms';
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,7 +7,7 @@ import { AppComponent }  from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule,FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent,HeroDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
